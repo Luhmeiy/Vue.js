5ab9a9adecb1715986aee519aaafdd1f3e620520
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2023-02-24 15:01:47 -0300

    Add responsive design

--- make-your-burger/src/components/Dashboard.vue
+++ make-your-burger/src/components/Dashboard.vue
@@ -84,7 +84,10 @@ onMounted(getOrders);
 		</div>
 
 		<div class="burger-table__rows">
-			<div class="burger-table__row" v-for="burger in burgers">
+			<div
+				class="burger-table__row burger-table__row--big"
+				v-for="burger in burgers"
+			>
 				<div class="order-number">{{ burger.id }}</div>
 				<div>{{ burger.name }}</div>
 				<div>{{ burger.bread }}</div>
@@ -96,7 +99,56 @@ onMounted(getOrders);
 					</ul>
 				</div>
 
+				<div class="controls">
+					<select
+						name="status"
+						class="status"
+						@change="($event) => updateBurger($event, burger.id)"
+					>
+						<option
+							:value="stts.type"
+							v-for="stts in status"
+							:selected="burger.status === stts.type"
+						>
+							{{ stts.type }}
+						</option>
+					</select>
+
+					<button class="delete-btn" @click="deleteBurger(burger.id)">
+						Cancelar
+					</button>
+				</div>
+			</div>
+
+			<div
+				class="burger-table__row burger-table__row--small"
+				v-for="burger in burgers"
+			>
+				<div class="order-number">#{{ burger.id }}</div>
+
 				<div>
+					<b>Cliente</b>
+					<p>{{ burger.name }}</p>
+				</div>
+
+				<div>
+					<b>Pão</b>
+					<p>{{ burger.bread }}</p>
+				</div>
+
+				<div>
+					<b>Carne</b>
+					<p>{{ burger.meat }}</p>
+				</div>
+
+				<div class="optional">
+					<b>Opcionais</b>
+					<ul>
+						<li v-for="opt in burger.optional">{{ opt }}</li>
+					</ul>
+				</div>
+
+				<div class="controls">
 					<select
 						name="status"
 						class="status"
@@ -121,31 +173,40 @@ onMounted(getOrders);
 </template>
 
 <style lang="scss" scoped>
-.burger-table {
-	margin: 0 auto;
-	min-width: 750px;
-	max-width: 1200px;
+@mixin wrap {
+	display: flex;
+	flex-wrap: wrap;
+}
 
-	&__heading,
-	&__rows,
-	&__row {
-		display: flex;
-		flex-wrap: wrap;
-	}
-
-	&__heading div,
-	&__row div {
+@mixin div-width {
+	& div {
 		width: 19%;
 	}
+}
+
+.burger-table {
+	margin: 0 auto;
+	max-width: 1200px;
 
 	&__heading {
 		border-bottom: 3px solid #333;
 		font-weight: bold;
 		padding: 12px;
 
+		@include wrap;
+		@include div-width;
+
+		@media screen and (max-width: 850px) {
+			display: none;
+		}
+
 		& .order-id {
 			width: 5%;
 		}
+	}
+
+	&__rows {
+		@include wrap;
 	}
 
 	&__row {
@@ -153,30 +214,85 @@ onMounted(getOrders);
 		padding: 12px;
 		width: 100%;
 
+		&--big {
+			@include wrap;
+			@include div-width;
+
+			@media screen and (max-width: 850px) {
+				display: none;
+			}
+
+			& .controls {
+				@media screen and (max-width: 1300px) {
+					align-items: center;
+					display: flex;
+					flex-direction: column;
+					row-gap: 10px;
+				}
+			}
+		}
+
+		&--small {
+			align-items: flex-start;
+			display: grid;
+			grid-template-columns: repeat(3, 1fr);
+			row-gap: 25px;
+
+			@media screen and (min-width: 851px) {
+				display: none;
+			}
+
+			@media screen and (max-width: 600px) {
+				grid-template-columns: repeat(2, 1fr);
+			}
+
+			& .optional {
+				@media screen and (min-width: 601px) {
+					grid-column: 1 / 3;
+				}
+			}
+		}
 		& .order-number {
 			width: 5%;
+
+			@media screen and (max-width: 850px) {
+				font-weight: bold;
+				grid-column: 1 / -1;
+			}
 		}
-	}
 
-	& select {
-		margin-right: 12px;
-		padding: 12px 6px;
-	}
+		& ul {
+			list-style-position: inside;
+		}
 
-	& .delete-btn {
-		background-color: #222;
-		border: 2px solid #222;
-		color: #fcba03;
-		cursor: pointer;
-		font-size: 16px;
-		font-weight: bold;
-		margin: 0 auto;
-		padding: 10px;
-		transition: 0.5s;
+		& .controls {
+			display: flex;
+			column-gap: 12px;
 
-		&:hover {
-			background-color: transparent;
-			color: #222;
+			@media screen and (max-width: 600px) {
+				grid-column: 1 / -1;
+				justify-self: center;
+			}
+
+			& select {
+				padding: 12px 6px;
+			}
+
+			& .delete-btn {
+				background-color: #222;
+				border: 2px solid #222;
+				color: #fcba03;
+				cursor: pointer;
+				font-size: 16px;
+				font-weight: bold;
+				padding: 10px;
+				transition: 0.5s;
+
+				&:hover {
+					background-color: transparent;
+					color: #222;
+				}
+			}
 		}
 	}
 }
